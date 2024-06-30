52a472e339ab99bfb733ecf20f37df5f853c12ac
s24010 <s24010@std.it-college.ac.jp>
2024-07-01 06:21:58 +0900

    entry_kadai.pyの再提出

--- entry_kadai.py
+++ entry_kadai.py
@@ -1,13 +1,15 @@
 #24010
-#受け取った名前に挨拶をする
+#エントリーウィジェットで受け付けた金額を税込み(10%)として出力
 
 import tkinter as tk #tkinterをtkと略して使用する
+import math
 
 def dispLabel():
     #entryメゾットを使用して入力を受け付け変数aに格納
     a = entry.get()
     print(f"aは{type(a)}") #ログの出方
-    lbl.config(text=f"{a}さんこんんちは")
+    b = int(a) * 1.1
+    lbl.config(text=f"税込み{math.floor(b)}円です")
 
 
 root = tk.Tk()
