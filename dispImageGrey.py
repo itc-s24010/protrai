5d5d74f392bcd7c85ea4da769733c811a9cee27b
s24010 <s24010@std.it-college.ac.jp>
2024-07-01 07:47:06 +0900

    課題提出

---
+++ dispImageGrey.py
@@ -0,0 +1,37 @@
+#24010
+#モノクロ画像に変換する
+
+
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image, PIL.ImageTk
+
+
+def dispPhoto(path):
+    newImage = PIL.Image.open(path).convert("L").resize((300, 300))
+    imageData = PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image = imageData)
+    imageLabel.image = imageData
+    lbl2 = tk.Label(text=path,font=("Helvetica", 16))
+    lbl2.pack()
+
+def openFile():
+    fpath = fd.askopenfilename()
+    if fpath:
+        dispPhoto(fpath)
+        print(fpath)
+
+root = tk.Tk()
+root.geometry("500x430")
+hensu = tk.Label(text="🎨画像表示アプリバージョン2.0🎨", font=("Helvetica", 20))
+apuri = tk.Label(text="開いていません", font=("Helvetica", 20))
+
+btn = tk.Button(text="ファイルを開く", command = openFile)
+imageLabel = tk.Label()
+
+hensu.pack()
+btn.pack()
+imageLabel.pack()
+apuri.pack()
+
+tk.mainloop()
